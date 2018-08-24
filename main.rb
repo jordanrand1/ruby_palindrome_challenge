f8e9464a61ea8901fa304bce36c38aaa2ca9da8d
Jordan Rand <jorrand@gmail.com>
2018-08-24 09:56:13 -0600

    finished product

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'colorize'
+
+def testChoice(input)
+  i = input.length
+  new_string = ''
+  while i > 0 do
+    i -= 1
+    new_string += input[i] 
+  end
+  puts new_string
+  if new_string.strip == input.strip
+    puts "You have entered a palindrome!".colorize(:green)
+    menu
+  else
+    puts "That was not a palindrome.".colorize(:red)
+    menu
+  end
+end
+
+def menu
+  puts "Please enter your text to see if it's a palindrome."
+  testChoice(gets.strip.to_s.delete(' ').downcase)
+end
+
+menu
